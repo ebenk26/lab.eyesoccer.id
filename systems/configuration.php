6a00bf8f66130a6f97698bcc69125e81f7649f4f
Kharizuno <kharizuno@yahoo.com>
2018-03-09 14:09:13 +0700

    config

--- systems/configuration.php
+++ systems/configuration.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['hostname'] 	= 'localhost';
+$config['hostname'] 	= '192.168.3.85';
 $config['username'] 	= ($_SERVER['SERVER_NAME'] == 'localhost') ? 'root' : 'dev_eyesoccer';
 $config['password'] 	= ($_SERVER['SERVER_NAME'] == 'localhost') ? '' : 'kaca-muka-siapakah-ini-2018';
 $config['database'] 	= ($_SERVER['SERVER_NAME'] == 'localhost') ? 'admin_eyesoback' : 'dev_eyesoccer';
