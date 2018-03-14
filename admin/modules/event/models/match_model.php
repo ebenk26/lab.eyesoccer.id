b60106ab807bc46f4a732a6fcffb48e46ed21ba5
Muhammad Robi <muhammadrobi65@gmail.com>
2018-03-14 21:13:33 +0700

    crud match
    --robi

---
+++ admin/modules/event/models/match_model.php
@@ -0,0 +1,86 @@
+<?php
+
+class Match_model extends CI_Model
+{
+
+    var $query_string = '';
+    var $command = '';
+    var $dtable = 'tbl_jadwal_event';
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    function __delete($id = '')
+    {
+        $option = $this->excurl->reqAction('match/delete', array('idx' => $id));
+        return $option;
+    }
+
+    function __disable($id = '')
+    {
+        if ($id != NULL) {
+            $dt = array('table' => $this->dtable, 'update' => array('is_active' => 0), 'where' => array('id_jadwal_event' => $id));
+            $option = $this->action->update($dt);
+
+            return $option;
+        } else {
+            redirect('match');
+        }
+    }
+
+    function __enable($id = '')
+    {
+        if ($id != NULL) {
+            $dt = array('table' => $this->dtable, 'update' => array('is_active' => 1), 'where' => array('id_jadwal_event' => $id));
+
+            $option = $this->action->update($dt);
+
+            return $option;
+        } else {
+            redirect('match');
+        }
+    }
+
+    function __path()
+    {
+        // Upload Path
+        $path = UPLOAD . 'eyevent/';
+
+        // Upload Config
+        $config = array(
+            'allowed_types' => 'gif|jpg|jpeg|png',
+            'max_size' => '1000',
+            'resize' => true
+        );
+
+        return array('path' => $path, 'resize' => true, 'config' => $config);
+    }
+
+    function __upload($newname = '')
+    {
+        $path = $this->__path();
+
+        $pic = '';
+        if ($this->input->post('Match_pic') != '') {
+            $pic = $this->input->post('Match_pic');
+        } else {
+            if ($this->input->post('temp_Match_pic') != '') {
+                $files = $this->input->post('temp_Match_pic');
+                $this->uploader->__unlink($path, $files);
+            }
+        }
+
+        $upload = $this->uploader->single_upload($path['config'], 'uploadfile', $path['path'], $pic, $newname);
+
+        return $upload;
+    }
+
+    function __unlink($post_pic = '')
+    {
+        $path = $this->__path();
+        $this->uploader->single_unlink($path['config'], 'uploadfile', $path['path'], $post_pic);
+    }
+
+}
---
+++ admin/modules/event/views/dreampro/match/match_table.php
@@ -0,0 +1,60 @@
+
+<table id='routertable'>
+    <tr>
+        <div id='showsort' value='event/search'></div>
+        <th style='width: 5%;'>
+            <input type='checkbox' name='checkall' id='checkall' onclick='actcheckall(this.id);'>
+        </th>
+        <th style='width: 10%;'>
+            <a href="javascript:void(0)" class='csort' id="logo" val="desc" onclick="actsort(this.id)">Event</a>
+        </th>
+        <th style='width: 20%;'>
+            <a href="javascript:void(0)" class='csort' id="logo" val="desc" onclick="actsort(this.id)">Logo</a>
+        </th>
+        <th>
+            <a href="javascript:void(0)" class='csort' id="title" val="desc" onclick="actsort(this.id)">Tim A</a>
+        </th>
+        <th class='hd-mobile' style='width: 5%;'>
+            <a href="javascript:void(0)" class='csort' id="publish_on" val="desc" onclick="actsort(this.id)">Score</a>
+        </th>
+        <th>
+            <a href="javascript:void(0)" class='csort' id="title" val="desc" onclick="actsort(this.id)">Tim B</a>
+        </th>
+        <th style='width: 20%;'>
+            <a href="javascript:void(0)" class='csort' id="logo" val="desc" onclick="actsort(this.id)">Logo</a>
+        </th>
+        <th style='width: 10%;'>Action</th>
+    </tr>
+    
+    <?php
+    // var_dump($dt);exit();
+    if($count->cc > 0)
+    {
+        $i= $offset;
+        foreach($dt as $r)
+        {
+            $pic_a = $this->library->picUrl($r->url_logo_a, $r->url_logo_a, 'eyevent', 'small');
+            $pic_b = $this->library->picUrl($r->url_logo_b, $r->url_logo_b, 'eyevent', 'small');
+
+            echo "<tr>";
+            echo "<td class='center'><input type='checkbox' name='selected[]' value='" . $r->id_event . "' class='ctab'></td>";
+            echo "<td class='center'><span>" . $r->event . "</span></td>";
+            echo "<td class='center'><div class='picproduct'><span style='background-image: url(" . $pic_a . ");'></span></div></td>";
+            echo "<td>" . $r->team_a . "</td>";
+            echo "<td>" . $r->score_a . " - " . $r->score_b . "</td>";
+            echo "<td>" . $r->team_b . "</td>";
+            echo "<td class='center'><div class='picproduct'><span style='background-image: url(" . $pic_b . ");'></span></div></td>";
+            echo "<td class='center'>
+                    <a class='btn_action' href='javascript:void(0)' onclick=\"openform('match/edit/". $r->id_jadwal_event ."')\" title='Edit'><i class='fa fa-edit fa-fw'></i></a>
+                    <a class='btn_action' href='javascript:void(0)' onclick=\"deleteid('match/delete/". $r->id_jadwal_event ."')\" title='Remove'><i class='fa fa-minus-square fa-fw'></i></a>
+                 </td>";
+            echo "</tr>";
+            
+            $i++;
+        }
+    } else {
+        echo "<tr><td colspan=5 style='text-transform: none;'>Data is not available</td></tr>";
+    }
+    
+    ?>
+</table>
