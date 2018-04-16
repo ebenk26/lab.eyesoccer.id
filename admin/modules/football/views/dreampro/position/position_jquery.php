64c482e94ff77a9ad89d19ba3bd5073b0c21109a
Chaliphs <michealiphs@gmail.com>
2018-04-16 14:08:33 +0700

    position

--- admin/modules/football/views/dreampro/position/position_jquery.php
+++ admin/modules/football/views/dreampro/position/position_jquery.php
@@ -1,13 +1,13 @@
 <div id='boxjq'>
     <div id='boxtable'>
-        <?php $this->load->view($this->config->item('base_theme').'/level/level_table'); ?>
+        <?php $this->load->view($this->config->item('base_theme').'/position/position_table'); ?>
     </div>
     
     <?php
         if($showpage > 1)
         {
             echo "<div id='pageself'>
-                    <div id='showurl' value='football/level/pagetable'></div>
+                    <div id='showurl' value='football/position/pagetable'></div>
                     <div id='showpage' value='$showpage'></div>
                     <div id='showoff' value='4'></div>
                     <div id='showrun' value='2'></div>
