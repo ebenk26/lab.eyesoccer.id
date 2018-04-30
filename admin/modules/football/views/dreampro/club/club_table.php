f9e5e599b51a565b6648fee4cc93502119ccc152
Kharizuno <kharizuno@yahoo.com>
2018-04-30 15:13:45 +0700

    'club

--- admin/modules/football/views/dreampro/club/club_table.php
+++ admin/modules/football/views/dreampro/club/club_table.php
@@ -4,26 +4,17 @@
         <th style='width: 5%;'>
             <input type='checkbox' name='checkall' id='checkall' onclick='actcheckall(this.id);'>
         </th>
-        <th style='width: 15%;'>
-            <a href="javascript:void(0)" class='csort' id="id_club" val="desc" onclick="actsort(this.id)">ID</a>
-        </th>
 		<th style='width: 15%;'>
-            <a href="javascript:void(0)" class='csort' id="image" val="desc" onclick="actsort(this.id)">Image</a>
+            <a href="javascript:void(0)" class='csort' id="id_club" val="desc" onclick="actsort(this.id)">Image</a>
         </th>
         <th>
             <a href="javascript:void(0)" class='csort' id="name" val="desc" onclick="actsort(this.id)">Club Name</a>
         </th>
-        <th class='hd-mobile' style='width: 15%;'>
-            <a href="javascript:void(0)" class='csort' id="establish_date" val="desc" onclick="actsort(this.id)">Establish Date</a>
-        </th>
 		<th class='hd-mobile' style='width: 15%;'>
-            <a href="javascript:void(0)" class='csort' id="last_update" val="desc" onclick="actsort(this.id)">Last Update</a>
+            <a href="javascript:void(0)" class='csort' id="id_competition" val="desc" onclick="actsort(this.id)">Competition</a>
         </th>
-		<th class='hd-mobile' style='width: 15%;'>
-            <a href="javascript:void(0)" class='csort' id="is_active" val="desc" onclick="actsort(this.id)">Status</a>
-        </th>
-		<th class='hd-mobile' style='width: 15%;'>
-            <a href="javascript:void(0)" class='csort' id="id_admin" val="desc" onclick="actsort(this.id)">Admin</a>
+		<th class='hd-mobile' style='width: 10%;'>
+            <a href="javascript:void(0)" class='csort' id="is_active" val="desc" onclick="actsort(this.id)">Active</a>
         </th>
         <th style='width: 10%;'>Action</th>
     </tr>
@@ -39,13 +30,10 @@
 
             echo "<tr>";
             echo "<td class='center'><input type='checkbox' name='selected[]' value='" . $r->id_club . "' class='ctab'></td>";
-			echo "<td>" . $r->id_club ."</td>";
             echo "<td class='center'><div class='picproduct'><span style='background-image: url(" . $pic . ");'></span></div></td>";
-            echo "<td>" . $r->name ."</td>";
-            echo "<td class='center capital hd-mobile'>" . $r->establish_date . "</td>";
-            echo "<td class='center capital hd-mobile'>" . $r->last_update . "</td>";
-            echo "<td class='center capital hd-mobile'>" . ($r->is_active == 1 ? 'Active' : 'Not Active'). "</td>";
-            echo "<td class='center capital hd-mobile'>" . $r->id_admin . "</td>";
+            echo "<td>" . $r->name ."<br>Phone : $r->phone<br>Establish Date : " . date('d M Y', strtotime($r->establish_date)) . "</td>";
+            echo "<td class='center capital hd-mobile'>" . $r->competition . "</td>";
+            echo "<td class='center capital hd-mobile'>" . $this->enum->active_string($r->is_active) . "</td>";
             echo "<td class='center'>
 					<a class='btn_action mg-r5' href='javascript:void(0)' onclick=\"actmenu('football/clubcareer/view/?id=". $r->id_club ."')\" title='View'><i class='fa fa-search fa-fw'></i></a>
                     <a class='btn_action' href='javascript:void(0)' onclick=\"openform('football/club/edit/". $r->id_club ."')\" title='Edit'><i class='fa fa-edit fa-fw'></i></a>
@@ -56,7 +44,7 @@
             $i++;
         }
     } else {
-        echo "<tr><td colspan=5 style='text-transform: none;'>Data is not available</td></tr>";
+        echo "<tr><td colspan=8 style='text-transform: none;'>Data is not available</td></tr>";
     }
     
     ?>
